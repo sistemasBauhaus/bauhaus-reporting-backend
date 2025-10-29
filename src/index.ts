3ccf6a758d15fec1e896f80eec4aa67b15d8ca97
sofiacostamagna <sofiacostamagna45l@gmail.com>
2025-10-29 11:27:54 -0300

    changes

--- src/index.ts
+++ src/index.ts
@@ -48,7 +48,10 @@ app.get('/', (_req, res) => {
 if (process.env.NODE_ENV === 'production') {
   const buildPath = path.join(__dirname, '../build');
   app.use(express.static(buildPath));
-  app.get('/*', (req, res) => {
+  app.get('/:any*', (req, res) => {
+    res.sendFile(path.join(buildPath, 'index.html'));
+  });
+  app.get('/:any*', (req, res) => {
     res.sendFile(path.join(buildPath, 'index.html'));
   });
 }
